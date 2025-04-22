7e7ce7aed708455152c99de0ad4211a99573d6e4
Victor Lomeli <victorlomeli2023@gmail.com>
2025-04-22 10:44:09 -0700

    minor changes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 lottie.loadAnimation({
     container: document.getElementById('animation'), // the DOM element
     renderer: 'svg',
-    loop: true,
+    loop: false,
     autoplay: true,
     path: 'lotti.json' // path to your animation JSON
   });
