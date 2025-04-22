7094f9e0b4872b8037355deee3326e8039335a95
Victor Lomeli <victorlomeli2023@gmail.com>
2025-04-22 10:30:16 -0700

    lotti animation is working

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+lottie.loadAnimation({
+    container: document.getElementById('animation'), // the DOM element
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'lotti.json' // path to your animation JSON
+  });
+  
